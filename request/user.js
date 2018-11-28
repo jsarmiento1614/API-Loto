5d520d12ad3cebcdd5f08df4e6a21098c970e28e
falbertoayala <32724150+falbertoayala@users.noreply.github.com>
2018-11-28 13:09:07 -0600

    Post de para crear y logueo de usuario
    Post que crear y loguea el usuario

--- request/user.js
+++ request/user.js
@@ -3,4 +3,70 @@ module.exports = (app, sql, sqlconfig) => {
     app.get('/', (req, res) => {
         res.send('<h1>BIENVENIDO A LA LOTELSA</h1>')
     });
+
+    //Post que Crea el Usuario
+
+app.Post("/v1/user/create",(req, res, next) =>{
+        
+    var user = req.body.user;
+    var password = req.body.password;
+    
+
+    if(!user &&  !password){
+        res.send("error");
+    }
+
+    var q = `insert into dbo.Users(UserName, UserPassword) values ('${user}','${password}')`;
+     
+    new sql.ConnectionPool(sqlConfig).connect().then(pool => {
+        return pool.query(q)
+    })
+    .then(result => {
+        var data = {
+            success: true,
+            message: `Se ha registrado el usuario ${user} `
+        }
+        res.send(data);
+    })
+    .catch(err => {
+        console.error(err);
+    })
+});
+
+//Post que Loguea al Usuario y le devuelve un Token
+
+app.post("/v1/user/login",(req, res, next)=>{
+    var user = req.body.user;
+    var password = req.body.password
+
+    if(!user || !password){
+        res.status(403).send({ message: "missing parameters"});
+    }
+
+    var q=  `select top 1 * from dbo.Users u where u.UserName = '${user}' and u.UserPassword = '${password}'`
+    
+    new sql.ConnectionPool(sqlConfig).connect().then(request => {
+        return request.query(q);
+    })
+    .then(result => {
+
+        if(result.recordset.length > 0)
+        {
+            res.send({ 
+                    success: true, 
+                    message: "",
+                    token: auth.CreateToken(result.recordset.UserId),
+                    user: result.recordset
+                });
+        } else {
+            res.status(403).send({
+                success: false,
+                message:"Error de usuario o password"
+            })
+        }
+    })
+    .catch(err =>{
+        return next(err);
+    })
+})
 }
